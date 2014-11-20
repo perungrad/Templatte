ed4d3f563055da9197838936c188112abaf798ca
pedzed <pedzed@outlook.com>
2014-11-20 23:16:28 +0100

    Now using views/home.php.

--- tests/index.php
+++ tests/index.php
@@ -4,9 +4,14 @@ require_once('../templatte.php');
 
 $template = new Templatte('main');
 
+ob_start();
+require('views/home.php');
+$content = ob_get_contents();
+ob_clean();
+
 $template->bind(array(
     'title'  => 'Templatté test',
-    'content' => 'You are home! :)'
+    'content' => $content,
 ));
 
 echo $template;
